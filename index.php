5dac38a24d156846ffdcc70afe5de7906b04f064
GLeoni17 <gabriel.lduarte@outlook.com>
2020-02-19 17:33:06 -0300

    6a versao - Adicionado index.php em exercicios

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
 	</head>
 	<body>
 		<h1>Pagina com links dos meus exercicios.</h1>
-		<a href="/20200219/index.php" />
+		<a href="20200219/Barbearia/index.php">Exercicio Barbearia</a>
 	</body>
 </html>
