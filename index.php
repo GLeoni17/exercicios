763494e20e821a503b9856233a93517259c71730
GLeoni17 <gabriel.lduarte@outlook.com>
2020-02-19 17:30:08 -0300

    6a versao - Adicionado index.php em exercicios

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h1>Pagina com links dos meus exercicios.</h1>
+		<a href="/20200219/index.php" />
+	</body>
+</html>
