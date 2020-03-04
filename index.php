6b798aef89acf077e5f32d43f5f3fc0290efa661
GLeoni17 <gabriel.lduarte@outlook.com>
2020-03-04 17:39:10 -0300

    Versao 8 - Editado link do index.php

--- index.php
+++ index.php
@@ -6,5 +6,6 @@
 	<body>
 		<h1>Pagina com links dos meus exercicios.</h1>
 		<a href="20200219/Barbearia/index.php">Exercicio Barbearia</a>
+		<a href="20200304/ex1.html">Exercicio JavaScript 04/03/2020</a>
 	</body>
 </html>
