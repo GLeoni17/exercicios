e992f13f26a9213b268fde1691ce6c427ef6b0b3
GLeoni17 <gabriel.lduarte@outlook.com>
2020-03-11 17:01:43 -0300

    Versao 11 - Editado index.php e adicionado exercicios do dia 11/03/2020

--- index.php
+++ index.php
@@ -8,5 +8,7 @@
 		<a href="20200219/Barbearia/index.php">Exercicio Barbearia</a>
 		<br>
 		<a href="20200304/ex1.html">Exercicio JavaScript 04/03/2020</a>
+		<br>
+		<a href="20200311/jogo_velha.html">Exercicio Jogo da Velha 11/03/2020</a>
 	</body>
 </html>
